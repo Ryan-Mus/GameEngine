7aa0cc4e2d2cd5f63ab3f082184d682bc48d7584
Ryan Mus <153521842+Ryan-Mus@users.noreply.github.com>
2025-06-13 12:46:05 +0200

    Fixed bug
    Bug fixed where play died multiple times when dying at the sport where de ghosts spawn.

--- MsPacman/LivesUIComponent.cpp
+++ MsPacman/LivesUIComponent.cpp
@@ -8,6 +8,7 @@
 #include "CustomPacmanDefines.h"
 
 #include <ServiceLocator.h>
+#include <TimeSingleton.h>
 
 
 void LivesUIComponent::Render() const
@@ -16,6 +17,19 @@ void LivesUIComponent::Render() const
 	{
 		float offsetX = i * 16.f * PACMAN_RENDERSCALE;
 		dae::Renderer::GetInstance().RenderTexture(*m_pTexture, GetOwner()->GetWorldPosition().x + offsetX, GetOwner()->GetWorldPosition().y,472, 0, 16, 16, PACMAN_RENDERSCALE);
+	}
+}
+
+void LivesUIComponent::Update()
+{
+	if (m_IsImmune)
+	{
+		m_ImmuneTimer += dae::Time::GetInstance().GetDeltaTime();
+		if (m_ImmuneTimer >= m_ImmuneDuration)
+		{
+			m_IsImmune = false;
+			m_ImmuneTimer = 0.f;
+		}
 	}
 }
 
@@ -28,4 +42,14 @@ void LivesUIComponent::LoseLife()
 void LivesUIComponent::ResetLives()
 {
 	m_Lives = 3;
+}
+
+void LivesUIComponent::OnNotify(MsPacmanEvent e)
+{
+	if (m_IsImmune) return;
+	if (e == MsPacmanEvent::DIE)
+	{
+		LoseLife();
+		m_IsImmune = true;
+	}
 }
--- MsPacman/LivesUIComponent.h
+++ MsPacman/LivesUIComponent.h
@@ -4,7 +4,7 @@
 #include "TextureComponent.h"
 #include "GameObject.h"
 
-class LivesUIComponent : public dae::Component, public MsPacmanObserver
+class LivesUIComponent final : public dae::Component, public MsPacmanObserver
 {
 public:
 	LivesUIComponent(dae::GameObject* pOwner):Component(pOwner) 
@@ -14,14 +14,20 @@ public:
 
 	void Render() const override;
 
+	void Update() override;
+
 	void LoseLife();
 	void ResetLives();
 	int GetLives() const { return m_Lives; }
 
-	void OnNotify(MsPacmanEvent e) override { if(e == MsPacmanEvent::DIE) LoseLife(); }
+	void OnNotify(MsPacmanEvent e) override;
 private:
 	int m_Lives = 3;
 	dae::Texture2D* m_pTexture{ nullptr };
+
+	bool m_IsImmune{ false };
+	float m_ImmuneTimer{0.f};
+	float m_ImmuneDuration{ 2.f };
 };
 
 
